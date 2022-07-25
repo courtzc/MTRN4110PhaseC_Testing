5d2903e7824803aade945d9f99a41a7a11c74da5
courtzc <32031285+courtzc@users.noreply.github.com>
2022-07-25 15:24:01 +1000

    basic autotester for as many worlds and photosets as we like. Huzzah!

--- auto_tester.py
+++ auto_tester.py
@@ -1,15 +1,19 @@
+from ntpath import join
 import os
 import numpy as np
+import os.path
+from project_function_code import project_function
+
+# ---
+# note: use the command palette to set the interpreter as python 3.7.10
+# ---
 
 # -----------------------------------------------------------------------------
 
 ### CHANGE THESE INPUTS ###
 
-# what is the path to your test folder?
-path_test = 'C:\\Users\\Court\\OneDrive\\Documents\\AAUNSW\\2022 T2\\MTRN4110\\MTRN4110PhaseC\\testing'
-
-# what is the path to your project folder?
-path_project = 'C:\\Users\\Court\\OneDrive\\Documents\\AAUNSW\\2022 T2\\MTRN4110\\MTRN4110PhaseC\\programs'
+# what is the path to your tests folder?
+path = 'C:\\Users\\Court\\OneDrive\\Documents\\AAUNSW\\2022 T2\\MTRN4110\\MTRN4110PhaseC\\tests'
 
 # how many worlds are you testing?
 x = 1
@@ -27,26 +31,45 @@ for j in range(1,t):
     
     h = str(j).zfill(3)
     
-    files = [i for i in os.listdir(path_test) if os.path_test.isfile(os.path_test.join(path_test,i)) and i.startswith(h)]
+    files = [i for i in os.listdir(path) if os.path.isfile(os.path.join(path,i)) and i.startswith(h)]
 
-    num_tests_for_maze = np.floor((len(files) - 1) / 3).astype(int)
-
-    Map_Expected = files[0]
+    # read in expected output .txt
+    map_file_temp = ["tests/", files[0]]
+    map_file = ''.join(map_file_temp)
+    
+    map_ex = ""
+    with open(map_file, 'r') as f:
+        line = f.readline()
+        while line:
+            map_ex+=str(line)
+            line = f.readline()
 
     # for each test set
     k = 1
     while k < len(files):
-        # take the three files
-        bug_photo = files[k]
-        maze_photo = files[k + 1]
-        robot_photo = files[k + 2]
         
-        # feed them into your program as inputs
+        # take the three files
+        bugfile =  ["tests/", files[k]]
+        mazefile =  ["tests/", files[k+1]]
+        robotfile =  ["tests/", files[k+2]]
+        bug_photo = ''.join(bugfile)
+        maze_photo = ''.join(mazefile)
+        robot_photo = ''.join(robotfile)
+        
+        # feed them into the project as inputs
+        map_generated = project_function(bug_photo, maze_photo, robot_photo)
         
         # compare output to the output text file
-        
         # if identical, yay!
+        if (map_generated == map_ex):
+            print("success! in set ", bug_photo[9:12])
+            
+        # if not identical, sad.
+        else:
+            print("failure in set ", bug_photo[9:12])
+            print("you generated: ")
+            print(map_generated)
+            print("but the expected output was: ")
+            print(map_ex)
         
-        # if not identical, show error.
-        
-        k += 1;
+        k += 3;
---
+++ project_function_code.py
@@ -0,0 +1,10 @@
+# import required dependencies
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def project_function (bug_pic, maze_pic, robot_pic):
+    # put your code here, fill map_output
+    map_output = ""
+    return map_output
+
