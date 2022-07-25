9bfa0c028c706cd5a05a497e32d76407c5c5dacc
courtzc <32031285+courtzc@users.noreply.github.com>
2022-07-25 17:29:52 +1000

    More verbose autotester

--- auto_tester.py
+++ auto_tester.py
@@ -28,8 +28,10 @@ t = x + 1
 
 # for each world
 for j in range(1,t):
-    
+
     h = str(j).zfill(3)
+    print("=================================")
+    print("Beginning tests for world", h, ".")
     
     files = [i for i in os.listdir(path) if os.path.isfile(os.path.join(path,i)) and i.startswith(h)]
 
@@ -47,7 +49,7 @@ for j in range(1,t):
     # for each test set
     k = 1
     while k < len(files):
-        
+     
         # take the three files
         bugfile =  ["tests/", files[k]]
         mazefile =  ["tests/", files[k+1]]
@@ -56,20 +58,30 @@ for j in range(1,t):
         maze_photo = ''.join(mazefile)
         robot_photo = ''.join(robotfile)
         
+        set_letter = bug_photo[9:12]
+        print("-----------------------")
+        print("Beginning test set", set_letter)
+        
         # feed them into the project as inputs
         map_generated = project_function(bug_photo, maze_photo, robot_photo)
         
         # compare output to the output text file
         # if identical, yay!
         if (map_generated == map_ex):
-            print("success! in set ", bug_photo[9:12])
+            print("success! in set ", set_letter)
             
         # if not identical, sad.
         else:
-            print("failure in set ", bug_photo[9:12])
+            print("failure in set ", set_letter)
             print("you generated: ")
             print(map_generated)
             print("but the expected output was: ")
             print(map_ex)
         
+        print("End of set", set_letter, "tests.")
+        print("---------------------")
+        
         k += 3;
+        
+    print("End of world", h, "tests.")
+    print("==========================")
